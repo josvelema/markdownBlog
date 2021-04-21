1873c303012e9fbed4e79c2e55660ecb1749206a
josvelema <volumensolutions@gmail.com>
2021-04-21 17:46:00 +0200

    completed tutorial but gonna redo it to add comments/notes for better understanding

--- routes/articlesOne.js
+++ routes/articlesOne.js
@@ -20,15 +20,25 @@ router.get('/:slug' , async(req ,res) => {
 })
  
 
-router.post('/', async (req,res, next) => {
-  req.article = new Article()
-  next()
-},saveArticleAndRedirect('new'))
+router.post('/', async (req,res) => {
+  let article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  })
 
-router.put('/:id', async (req,res, next) => {
-  req.article = await Article.findById(req.params.id)
-  next()
-},saveArticleAndRedirect('edit'))
+  try {
+    article = await article.save()
+  res.redirect(`/articles/${article.slug}`)
+  } catch (error) {
+    console.log('error !!!!!' + error);
+    res.render('articles/new', { article: article })
+  }
+})
+
+router.put('/:id', (req,res) => {
+
+})
 
 
 router.delete('/:id', async (req,res) => {
@@ -37,18 +47,8 @@ router.delete('/:id', async (req,res) => {
 })
 
 function saveArticleAndRedirect(path) {
-  return async (req,res) => {
-    let article = req.article
-      article.title = req.body.title
-      article.description = req.body.description
-      article.markdown = req.body.markdown
-    try {
-      article = await article.save()
-    res.redirect(`/articles/${article.slug}`)
-    } catch (error) {
-      console.log('error !!!!!' + error);
-      res.render(`articles/${path}`, { article: article })
-    }
+  return (req,res) => {
+    
   }
 }
 
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 // database connect via mongoose
 
-mongoose.connect('mongodb://localhost/blog' ,{ 
+mongoose.connect('mongodb://127.0.0.1/blog' ,{ 
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true
  })
 
 // views
@@ -16,18 +19,25 @@ app.set('view engine','ejs')
 
 //routes
 
+app.use(express.urlencoded({ extended: false }))
+
+app.use(methodOverride('_method'))
+
 app.use('/articles', articleRouter)
 
-app.get('/', (req,res) => {
-  const articles = [{
-    title: 'test art',
-    created: new Date(),
-    description: 'test description'
-  },{
-    title: 'test art2',
-    created: new Date(),
-    description: 'test description'
-  }]
+
+app.get('/', async (req,res) => {
+  // const articles = [{
+  //   title: 'test art',
+  //   created: new Date(),
+  //   description: 'test description'
+  // },{
+  //   title: 'test art2',
+  //   created: new Date(),
+  //   description: 'test description'
+  // }]
+  const articles = await Article.find().sort({created: 'desc'})
+
   res.render('articles/index', { articles })
 })
 
